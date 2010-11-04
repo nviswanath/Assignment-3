da083d1079834287efca4ae663152a87be03489b
vishu <vishu@ece.unm.edu>
2010-11-04 00:07:25 -0600

    This is Assignment 3 from ECE595 Cloud Computing

--- Assignment3/app/models/participant.rb
+++ Assignment3/app/models/participant.rb
@@ -1,2 +1,8 @@
 class Participant < ActiveRecord::Base
+   
+      validates_format_of :email,
+   
+     			   :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+   
+     			   :message => 'email must be valid'
 end
